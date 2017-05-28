69397986ca95a48d9af636b84b90224a9978fe90
Gampesh <gampesh@gmail.com>
2017-05-28 12:06:51 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,4 +1,18 @@
-def mul(a,b):
-    return a*b
-mul(1,2)
+import math_op
+import unittest
 
+class TestMathOP(object):
+
+
+    def test_add_op():
+        assert math_op.add(10, 20) == 30
+
+    def test_mul_op():
+        assert math_op.mul(10, 20) == 200
+
+    def test_mul_op():
+        assert math_op.mul(10, 20) == 20
+
+
+if __name__ == '__main__':
+    unittest.main()
